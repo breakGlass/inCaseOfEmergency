b8faae4dd0b1d8b7ad62815cd679da1958187d45
KeriPMiller <miller.keripatricia@gmail.com>
2018-01-20 14:56:33 -0500

    added webpack and babel configs, not fully working yet

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './client/index.js', // the starting point for our program
+  output: {
+    path: __dirname + '/public', // the absolute path for the directory where we want the output to be placed
+    filename: 'bundle.js' // the name of the file that will contain our output - we could name this whatever we want, but bundle.js is typical
+  },
+  module: {
+    rules: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          cacheDirectory: true,
+        },
+      }, ],
+    }, ],
+  },
+  devtool: "source-map",
+}
