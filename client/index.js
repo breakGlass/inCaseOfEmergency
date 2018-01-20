b161264c681d1ef1689e3a551439bfe845f84b2f
Shannon Kendall <snkendall@gmail.com>
2018-01-20 15:46:08 -0500

    branch weirdness hold please

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Main } from './components';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Main />
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
