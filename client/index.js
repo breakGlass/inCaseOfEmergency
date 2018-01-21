bddd83279aacc593e851f1d037562b7b071881e8
Shannon Kendall <snkendall@gmail.com>
2018-01-20 20:19:12 -0500

    set up markers and marker functions

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-// const api = require("./api");
-// const buildMarker = require("./marker.js");
+ const api = require('./api');
+ const buildMarker = require('./marker.js');
 
 //Instantiate the Map
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3luZzI0IiwiYSI6ImNqY25ydGZkdzFscGoyd3JuZjZnY2h6eDEifQ.87cz_oxJm1pma_aWv5lI4A';
